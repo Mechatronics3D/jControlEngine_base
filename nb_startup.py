738a5fcf821e59596032ba65bd3434e76d4957c5
Behzad Samadi <behzad.samadi@gmail.com>
2016-10-22 22:59:09 -0400

    Update nb_startup.py

--- nb_startup.py
+++ nb_startup.py
@@ -1,6 +1,7 @@
 %matplotlib inline
 import sys
+import os
 if sys.version_info < (3, 0):
-    sys.path.append(r"CASADIPATH2")
+    sys.path.append(os.environ['CASADIPATH2'])
 else:
-    sys.path.append(r"CASADIPATH3")
+    sys.path.append(os.environ['CASADIPATH3'])
